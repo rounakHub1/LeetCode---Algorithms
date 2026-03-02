55cc43204f156ad738550d5c30dd95655ec5f284
Rounak Banerjee <rounakbanerjee2.2005@gmail.com>
2026-03-03 04:21:37 +0530

    Time: 3 ms (77.35%) | Memory: 46.3 MB (94.43%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+        char prev = s.charAt(0);
+        int num=0;
+        for(char digit : s.toCharArray()) {
+
+            if(value(prev) < value(digit)) {
+                num = num + value(digit) - 2 * value(prev);
+            } else {
+                num = num + value(digit);
+            } prev = digit;
+        }
+        return num;
+    }
+    private int value(char digit) {
+
+        switch(digit) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
