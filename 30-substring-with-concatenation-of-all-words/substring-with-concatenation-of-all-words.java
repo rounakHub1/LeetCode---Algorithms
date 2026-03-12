4f1395686a214cc413e7bd3e994c8f3c71bc0c2d
Rounak Banerjee <rounakbanerjee2.2005@gmail.com>
2026-03-13 04:40:01 +0530

    Time: 12 ms (59.89%) | Memory: 47.1 MB (85.06%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> result = new ArrayList<>();
@@ -10,13 +8,10 @@ class Solution {
         int totalLen = wordLen * wordCount;
         int sLen = s.length();
 
-        // Step 1: Build the reference frequency map
         Map<String, Integer> wordFreq = new HashMap<>();
         for (String w : words) {
             wordFreq.put(w, wordFreq.getOrDefault(w, 0) + 1);
         }
-
-        // Step 2: Run the window starting from each possible offset (0 to wordLen - 1)
         for (int i = 0; i < wordLen; i++) {
             int left = i;
             int right = i;
@@ -31,27 +26,22 @@ class Solution {
                     seen.put(word, seen.getOrDefault(word, 0) + 1);
                     count++;
 
-                    // If we have more of 'word' than allowed, slide 'left' to the right
                     while (seen.get(word) > wordFreq.get(word)) {
                         String leftWord = s.substring(left, left + wordLen);
                         seen.put(leftWord, seen.get(leftWord) - 1);
                         count--;
                         left += wordLen;
                     }
-
-                    // If count matches total words, we found a valid index
                     if (count == wordCount) {
                         result.add(left);
                     }
+
                 } else {
-                    // Word not in dictionary, reset window
                     seen.clear();
                     count = 0;
                     left = right;
                 }
             }
-        }
-
-        return result;
+        } return result;
     }
 }
