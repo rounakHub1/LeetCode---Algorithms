4147d8e311f249c06cc4cd75d37108e17a322986
Rounak Banerjee <rounakbanerjee2.2005@gmail.com>
2026-03-17 02:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (81.18%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,40 +1,37 @@
 class Solution {
-    
     public void nextPermutation(int[] nums) {
-        
-        int ln = nums.length;
-        int i = ln - 1;
+        int n = nums.length;
+        int i = n - 2;
 
-        while (i > 0) {
-            if (nums[i] > nums[i - 1]) {
-                
-                int pivotIndex = i - 1;
-                int successorIndex = ln - 1;
-                
-                while (nums[successorIndex] <= nums[pivotIndex]) {
-                    successorIndex--;
-                }
-
-                int temp = nums[pivotIndex];
-                nums[pivotIndex] = nums[successorIndex];
-                nums[successorIndex] = temp;
-
-                int start = i;
-                int end = ln - 1;
-
-                while (start < end) {
-                    int t = nums[start];
-                    nums[start] = nums[end];
-                    nums[end] = t;
-                    start++;
-                    end--;
-                }
-                return;
-            } 
+        // 1. Find the first decreasing element from the right (the pivot)
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-        if (i == 0) {
-            Arrays.sort(nums);
+
+        if (i >= 0) {
+            // 2. Find the element just larger than the pivot to swap with
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // 3. Reverse the elements to the right of the pivot
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
         }
     }
 }
