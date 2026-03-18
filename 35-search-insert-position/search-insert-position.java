99a2f3118f3aa6507875515946d4ea3eb935d811
Rounak Banerjee <rounakbanerjee2.2005@gmail.com>
2026-03-19 03:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (62.25%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,11 +3,15 @@ class Solution {
         
         int left = 0;
         int right = nums.length - 1;
+
         while(left <= right) {
+
             int mid = left + (right - left) / 2;
+
             if(nums[left] == target) return left;
             if(nums[right] == target) return right;
             if(nums[mid] == target) return mid;
+            
             if(nums[mid] > target) right = mid - 1;
             else if(nums[mid] < target) left = mid + 1;
         }
